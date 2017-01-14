8db7eb47b347784872ec5aaa6b059b713f1b633b
Zach Temkin <zachtemkin@us.ibm.com>
2017-01-14 11:37:46 -0600

    updates style of project navigation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,9 @@
-var $menu = document.getElementById('menu');
-var $wrapper = document.getElementById('menu-wrapper');
-var $ul = document.getElementById('main-nav ul');
-
-$menu.on('click', function() {
-  $wrapper.toggleClass('is-clicked');
-  $ul.toggleClass('is-expanded');
-});
-
 var feed = new Instafeed({
   get: 'user',
   userId: 12532684,
   clientId: '8711b028c8b84f57882bb00336488d00',
   accessToken: '12532684.8711b02.5a40ec50407745e68341f0f403dacb52',
-  template: '<a class="zt-gram" href="{{link}}"><img src="{{image}}" /> <ul class="info-container"><li class="likes">{{likes}}</li><li class="location">{{location}}</li><li class="caption">{{caption}}</li></ul></a>',
+  template: '<a class="zt-gram" href="{{link}}"><img src="{{image}}" /> <div class="info-container"><p class="likes">{{likes}}</p><p class="location">{{location}}</p><p class="insta-caption">{{caption}}</p></div></a>',
   limit: 4,
   resolution: 'standard_resolution'
 });
